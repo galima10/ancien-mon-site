1d792742df7539090db1edb310f181786babb774
Magali <maimagali26.pro@gmail.com>
2023-08-12 17:13:57 +0200

    maj page ouvrage

--- javascript/bibliothèque.js
+++ javascript/bibliothèque.js
@@ -1,23 +1,69 @@
-var p = 0
-document.querySelector("#b_d").addEventListener("click", ()=>{
-    if (p>=2){
+// Slider 1
+
+var p1 = 0
+document.querySelector("#b_d1").addEventListener("click", ()=>{
+    if (p1>=2){
     
     } else {
-        $("#slides").finish().animate({
-            left: "-=1000"         
+        $("#slides1").finish().animate({
+            left: "-=95vw"         
     });
-    p=p+1
+    p1=p1+1
     }
-    console.log(p)
 })
-document.querySelector("#b_g").addEventListener("click", ()=>{
-    if (p<=0){
+document.querySelector("#b_g1").addEventListener("click", ()=>{
+    if (p1<=0){
     
     } else {
-        $("#slides").finish().animate({
-            left: "+=1000"         
+        $("#slides1").finish().animate({
+            left: "+=95vw"         
     });
-    p=p-1
+    p1=p1-1
     }
-    console.log(p)
+})
+
+// Slider 2
+
+var p2 = 0
+document.querySelector("#b_d2").addEventListener("click", ()=>{
+    if (p2>=2){
+    
+    } else {
+        $("#slides2").finish().animate({
+            left: "-=95vw"         
+    });
+    p2=p2+1
+    }
+})
+document.querySelector("#b_g2").addEventListener("click", ()=>{
+    if (p2<=0){
+    
+    } else {
+        $("#slides2").finish().animate({
+            left: "+=95vw"         
+    });
+    p2=p2-1
+    }
+})
+
+// Slider 1 : Slide 1
+
+document.querySelector("#Diap1Plus1").addEventListener("mouseover", ()=>{
+    document.querySelector("#diap1desc1").classList.replace("h_desc1", "h_desc2");
+    
+})
+document.querySelector("#Diap1Plus1").addEventListener("mouseout", ()=>{
+    document.querySelector("#diap1desc1").classList.replace("h_desc2", "h_desc1");
+})
+
+// Slider 1 : Slide 2
+
+var click1_2 = 0
+
+document.querySelector("#Diap1Plus2").addEventListener("mouseover", ()=>{
+    document.querySelector("#diap1desc2").classList.replace("h_desc1", "h_desc2");
+    
+})
+document.querySelector("#Diap1Plus2").addEventListener("mouseout", ()=>{
+    document.querySelector("#diap1desc2").classList.replace("h_desc2", "h_desc1");
 })
