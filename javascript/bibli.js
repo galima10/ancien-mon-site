60c7a214a33f3540eca9a8a767dc138094827795
Magali <maimagali26.pro@gmail.com>
2023-08-14 00:45:00 +0200

    media queries ouvrage et bibli

--- javascript/bibli.js
+++ javascript/bibli.js
@@ -1,6 +1,17 @@
-document.querySelector("#enc1").addEventListener("mouseover", ()=>{
-    document.querySelector("#desc1").classList.replace("op0", "op1");
-})
-document.querySelector("#enc1").addEventListener("mouseout", ()=>{
-    document.querySelector("#desc1").classList.replace("op1", "op0");
+var click = 0
+
+document.querySelector("#enc1").addEventListener("click", ()=>{
+    if (click==0){
+        document.querySelector("#desc1").classList.remove("none");
+        setTimeout(()=>{
+            document.querySelector("#desc1").classList.replace("op0", "op1");
+        },"1") 
+        click=1
+    } else if (click==1){
+        document.querySelector("#desc1").classList.replace("op1", "op0");
+        setTimeout(()=>{
+            document.querySelector("#desc1").classList.add("none");
+        },"1501") 
+        click=0
+    }
 })
